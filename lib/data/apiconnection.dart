2924bf2743681d3c53b4293980539ea5311a698a
İsmail Babacan <114854598+ismailbabacann@users.noreply.github.com>
2024-07-23 14:20:52 +0300

    finding a way to list categories

--- lib/data/apiconnection.dart
+++ lib/data/apiconnection.dart
@@ -22,4 +22,45 @@ Future<List> fetchLatestMovies() async {
   } else {
     throw Exception('Failed to load latest movies');
   }
+
 }
+
+class Apicategories {
+  final String baseUrl = 'http://www.omdbapi.com/';
+  final String apiKey = '770267f9';
+
+  Future<List<Movie>> fetchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl?s=$query&apikey=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = json.decode(response.body);
+      if (jsonResponse['Response'] == 'True') {
+        List movies = jsonResponse['Search'];
+        return movies.map((movie) => Movie.fromJson(movie)).toList();
+      } else {
+        throw Exception('No movies found');
+      }
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
+}
+
+class Movie {
+  final String title;
+  final String poster;
+  final String year;
+
+  Movie({required this.title, required this.poster, required this.year});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      title: json['Title'],
+      poster: json['Poster'],
+      year: json['Year'],
+    );
+  }
+}
+
